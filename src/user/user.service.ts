7ed6e597ec5b04fecc9f6fd8b4294ff92e07e2c0
Radosław <radek.barylak@gmail.com>
2023-12-19 18:20:29 +0100

    Added veryfication of e-mail already existing in database

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 const bcrypt = require('bcrypt');
 import db from '../../db'
 import { v4 as uuid } from 'uuid';
@@ -9,6 +9,15 @@ export class UserService {
 
 	async createUser(user): Promise<User>{
 		
+		const usersWithSameEMail = await db('users').where({
+			e_mail: user.e_mail,
+			deleted_at: null
+		})
+
+		if(usersWithSameEMail.length){
+			throw new HttpException(`User with e-mail: '${user.e_mail}', already exists in the database.`, HttpStatus.CONFLICT);
+		}
+
 		const hashedPassword = await new Promise((resolve, reject) => {
 			bcrypt.hash(user.password, 10, (err, hash) => {
 				if (err) reject(err)
